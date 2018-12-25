05477215bdc7a25ae4d9b6eae982b448f5883754
GoldaAlexey <leha.golda@gmail.com>
2018-12-25 18:47:21 +0300

    Added activities

--- Code/app/src/main/java/by/bsuir/golda/moneymanager/view/MainActivity.java
+++ Code/app/src/main/java/by/bsuir/golda/moneymanager/view/MainActivity.java
@@ -1,5 +1,6 @@
-package by.bsuir.golda.moneymanager;
+package by.bsuir.golda.moneymanager.view;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -12,6 +13,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import by.bsuir.golda.moneymanager.R;
+import by.bsuir.golda.moneymanager.view.AccountActivity;
+import by.bsuir.golda.moneymanager.view.TransactionActivity;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -77,21 +82,21 @@ public class MainActivity extends AppCompatActivity
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
+        Intent intent;
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
+        if (id == R.id.nav_account) {
+            intent = new Intent(getApplicationContext(), AccountActivity.class);
+            startActivity(intent);
+        } else if (id == R.id.nav_transaction) {
+            intent = new Intent(getApplicationContext(), TransactionActivity.class);
+            startActivity(intent);
+        } else if (id == R.id.nav_transaction_plan) {
+            intent = new Intent(getApplicationContext(), TransactionPlanActivity.class);
+            startActivity(intent);
+        } else if (id == R.id.nav_category) {
+            intent = new Intent(getApplicationContext(), CategoryActivity.class);
+            startActivity(intent);
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
